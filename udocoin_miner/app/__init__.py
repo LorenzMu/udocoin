f2b33646971a1b14e24a489a090114786c5e1774
MatthiasHeilmann <78656019+MatthiasHeilmann@users.noreply.github.com>
2023-10-16 14:23:19 +0200

    added socketio & Dockerfile

--- udocoin_miner/app/__init__.py
+++ udocoin_miner/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
 CORS(app)
+
+socketio = SocketIO(app)
 
 from app import endpoints
