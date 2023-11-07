f67e2211f7b1898927143e91e41a2db7044f8162
MatthiasHeilmann <78656019+MatthiasHeilmann@users.noreply.github.com>
2023-11-07 10:51:56 +0100

    quickfixes deployment

--- udocoin_miner/start.sh
+++ udocoin_miner/start.sh
@@ -2,80 +2,100 @@
 
 # Function to prompt user and handle responses
 function prompt_for_keys {
-    read -p "Do you have a private and a public key? [y/n] " privateKeyResponse
+    read -p "Do you have a private and a public key? [y/n]" private_key_response
 
-    if [ "$privateKeyResponse" == "n" ]; then
-        if [ ! -f "application/venv/Scripts/Activate.ps1" ]; then
+    if [[ "$private_key_response" == "n" ]]; then
+        # If venv/Scripts/Activate.ps1 does not exist
+        if [ ! -f "application/venv/Scripts/activate" ]; then
             python -m venv application/venv
         fi
 
+        # Activate virtual environment
         source application/venv/Scripts/activate
 
+        # Install cryptography library
         pip install "cryptography==41.0.4"
-
+        
+        # Generate keys using key_gen.py
+        # private and public key will be saved to .udocoin
         python application/key_gen.py
 
+        # Deactivate virtual environment
         deactivate
 
-    elif [ "$privateKeyResponse" != "y" ]; then
+    elif [[ "$private_key_response" != "y" ]]; then
         echo "Invalid input. Please enter 'y' or 'n'."
-        prompt_for_keys
+        prompt_for_keys  # Ask again for valid input
     fi
 }
 
 function set_privkey_variable {
-    while true; do
-        read -p "Please insert the path to your private-key: " PRIVKEY_PATH
+    try {
+        PRIVKEY_PATH="$HOME/.udocoin/priv_key"
+        read -p "Please insert the path to your private-key or skip for default [$PRIVKEY_PATH]: " privkey_path_input
+        PRIVKEY_PATH="${PRIVKEY_PATH:-$privkey_path_input}"
 
         if [ ! -f "$PRIVKEY_PATH" ]; then
             echo "File not found: $PRIVKEY_PATH"
-        else
-            PRIVKEY_CONTENT=$(<$PRIVKEY_PATH)
-            export PRIVKEY_CONTENT
-            break
+            set_privkey_variable
         fi
-    done
+
+        PRIVKEY_CONTENT=$(cat "$PRIVKEY_PATH")
+        export PRIVKEY="$PRIVKEY_CONTENT"
+        return $PRIVKEY_CONTENT
+    } catch {
+        echo "Error: $_"
+    }
 }
 
 function set_pubkey_variable {
-    while true; do
-        read -p "Please insert the path to your public-key: " PUBKEY_PATH
+    try {
+        PUBKEY_PATH="$HOME/.udocoin/pub_key.pub"
+        read -p "Please insert the path to your public-key or skip for default [$PUBKEY_PATH]: " pubkey_path_input
+        PUBKEY_PATH="${PUBKEY_PATH:-$pubkey_path_input}"
 
         if [ ! -f "$PUBKEY_PATH" ]; then
             echo "File not found: $PUBKEY_PATH"
-        else
-            PUBKEY_CONTENT=$(<$PUBKEY_PATH)
-            export PUBKEY_CONTENT
-            break
+            set_pubkey_variable
         fi
-    done
+
+        PUBKEY_CONTENT=$(cat "$PUBKEY_PATH")
+        export PUBKEY="$PUBKEY_CONTENT"
+        return $PUBKEY_CONTENT
+    } catch {
+        echo "Error: $_"
+    }
 }
 
 function set_seed_server_variable {
-    while true; do
-        read -p "Is the Server supposed to run as a Seed-Server and has a static and public ip address? [y/n] " SEED_SERVER
+    read -p "Is the Server supposed to run as a Seed-Server and has a static and public IP address? [y/n]" seed_server
 
-        if [ "$SEED_SERVER" != "y" ] && [ "$SEED_SERVER" != "n" ]; then
-            echo "Invalid input. Please enter 'y' or 'n'."
-        else
-            export SEED_SERVER
-            break
-        fi
-    done
+    if [[ "$seed_server" != "y" && "$seed_server" != "n" ]]; then
+        echo "Invalid input. Please enter 'y' or 'n'."
+        set_seed_server_variable
+    fi
+
+    export SEED_SERVER="$seed_server"
+    return $seed_server
 }
 
-# Main Script
+try {
+    prompt_for_keys
 
-prompt_for_keys
+    # Get user inputs and set env variables
+    PRIVKEY_CONTENT=$(set_privkey_variable)
+    PUBKEY_CONTENT=$(set_pubkey_variable)
+    SEED_SERVER=$(set_seed_server_variable)
 
-set_privkey_variable
-set_pubkey_variable
-set_seed_server_variable
+    export PRIVKEY="$PRIVKEY_CONTENT"
+    export PUBKEY="$PUBKEY_CONTENT"
+    export SEED_SERVER="$SEED_SERVER"
 
-export PRIVKEY="$PRIVKEY_CONTENT"
-export PUBKEY="$PUBKEY_CONTENT"
+    # python test.py
 
-# python test.py
-
-# Build docker containers
-# docker-compose up --build
+    # Build docker containers
+    docker-compose up --build
+}
+catch {
+    echo "Error: $_"
+}
