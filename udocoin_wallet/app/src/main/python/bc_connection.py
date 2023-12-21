676b30d2ba68875b5e1381b1b2fb7a69ebae8ba5
MatthiasHeilmann <78656019+MatthiasHeilmann@users.noreply.github.com>
2023-12-21 12:54:23 +0100

    quickfix timeout wallet app

--- udocoin_wallet/app/src/main/python/bc_connection.py
+++ udocoin_wallet/app/src/main/python/bc_connection.py
@@ -1,5 +1,4 @@
 import requests
-import os,pathlib
 import json
 
 def seed_is_active(ip:str)->bool:
@@ -31,30 +30,19 @@ def get_known_seeds():
         print(response.text)
         raise Exception("Error getting seed list:", response.text)
 
-def post_transaction(transaction:str)->str:
-    transaction = json.load(transaction)
-    active_seeds = get_known_seeds()
-    for seed in active_seeds:
-        response = requests.post(
-            url=f"{seed}/miner/post_transaction",
-            json=transaction)
-        if response.status_code == 200 or response.status_code == 201:
-            return response.text
-    return None
-
 def get_balance_by_public_key(public_key:str)->float:
     active_seeds = get_known_seeds()
     for seed in active_seeds:
         try:
             key = public_key.replace('\n','_')
-            response = requests.get(f"{seed}/miner/get_balance?pubkey={key}")
+            response = requests.get(f"{seed}/miner/get_balance?pubkey={key}",timeout=5)
             if response.status_code == 200:
                 return str(response.text)
         except:
             pass
     return "N/a"
 
-def send_transaction(signed_transaction:str):
+def send_transaction(signed_transaction:str)->bool:
     print("Sending transaction... ")
     active_seeds = get_known_seeds()
     transaction = json.loads(signed_transaction)
